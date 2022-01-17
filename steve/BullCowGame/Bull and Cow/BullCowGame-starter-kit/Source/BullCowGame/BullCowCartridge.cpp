941f4f7791d7a0bd5a79b11388c26eceb2692759
stevenanton <steveanton78@gmail.com>
2022-01-16 23:11:41 -0500

    Added preliminary Game Loop

--- steve/BullCowGame/Bull and Cow/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ steve/BullCowGame/Bull and Cow/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -22,10 +22,24 @@ void UBullCowCartridge::OnInput(const FString& Input) // When the player hits en
         playerName = Input;
         welcome = false;
         PrintLine(TEXT("Howdy there, " + playerName + "!"));
+        PrintLine(TEXT("Here's how the game works:"));
+        PrintLine(TEXT("You need to guess the isogram! It's as simple as that!"));
+        PrintLine(TEXT("What's an isogram, you say? It's a word with no repeated letters."));
+        PrintLine(TEXT("Enter your guess to begin!"));
     }
     else
     {
-        FString hiddenWord = TEXT("bourbon");
+        FString hiddenWord = TEXT("ravens");
+        if (hiddenWord.Equals(Input))
+        {
+            PrintLine(TEXT("Wow! I didn't think it was possible!"));
+            PrintLine(TEXT("Awesome job getting the word!"));
+            PrintLine(TEXT("Come back again sometime!"));
+        }
+        else
+        {
+            PrintLine(TEXT("Nope! The correct word was: " + hiddenWord));
+        }
 
     }
     
