5f5b17fa790442666be751d1af9e7b6a445151e3
stevenanton <steveanton78@gmail.com>
2022-01-16 22:26:47 -0500

    Updated Welcome Message

--- steve/BullCowGame/Bull and Cow/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ steve/BullCowGame/Bull and Cow/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -5,7 +5,7 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
     PrintLine("Welcome to the Wonderful World of Cows!");
-    PrintLine("\"If you ain't having fun, you're full of bull!\"");
+    PrintLine("\"If you ain't having fun...\n you must be full of bull!\"");
     PrintLine("What's your name, buddy?");
 
 }
