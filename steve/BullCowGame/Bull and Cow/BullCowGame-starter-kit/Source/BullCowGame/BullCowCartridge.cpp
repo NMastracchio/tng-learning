73b812224154b8f9f0bbc68ad5eef444ac934072
stevenanton <steveanton78@gmail.com>
2022-01-16 22:25:01 -0500

    Added Welcome Message and ClearScreen()

--- steve/BullCowGame/Bull and Cow/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ steve/BullCowGame/Bull and Cow/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,13 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine("Welcome to the Wonderful World of Cows!");
+    PrintLine("\"If you ain't having fun, you're full of bull!\"");
+    PrintLine("What's your name, buddy?");
+
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
-
+    ClearScreen();
 }
